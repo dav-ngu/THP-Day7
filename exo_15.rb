413897bf3a5215ca04db7f837e27dc9beedc1839
davnguyen <dav-ngu31@hotmail.fr>
2022-01-18 16:25:29 +0100

    deuxième version

--- exo_15.rb
+++ exo_15.rb
@@ -1,13 +1,8 @@
 puts "Bienvenue, combien d'étages souhaites-tu dans la pyramide (entre 1 et 25) ?"
 x = gets.chomp.to_i
-y ="#"
 
-puts ""
-puts "Tu as choisi #{x} étages :"
-
-x.times do
-    puts y
-    y=y+"#"
+x.times do |index|
+    puts "#" *(index+1)
 end
 
 
--- exo_16.rb
+++ exo_16.rb
@@ -1,11 +1,6 @@
 puts "Bienvenue, combien d'étages souhaites-tu dans la pyramide (entre 1 et 25) ?"
 x = gets.chomp.to_i
-y ="#"
-z= " "
 
-puts ""
-puts "Tu as choisi #{x} étages :"
-x.times do
-    puts y
-    y=-z+y+"#"
+x.times do |index|
+    puts (" "*(x-index))+("#" *(index+1))
 end

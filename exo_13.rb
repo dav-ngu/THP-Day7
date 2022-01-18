3cc49232e0c3f2a0573180dd32884c5106b7c99f
davnguyen <dav-ngu31@hotmail.fr>
2022-01-18 18:09:05 +0100

    deuxième version

--- exo_13.rb
+++ exo_13.rb
@@ -4,11 +4,13 @@ y = gets.chomp.to_i
 
 y.times do |index|
 if (index < 10)
-    puts "jean.dupont.0#{index+1}@email.fr" 
+    z= "jean.dupont.0#{index+1}@email.fr" 
 else
-	puts "jean.dupont.#{index+1}@email.fr"
+	z= "jean.dupont.#{index+1}@email.fr"
 end
-z="jean.dupont.#{index+1}@email.fr"
+
 x.push(z)
 end
-  
+
+puts ""
+puts x
--- exo_14.rb
+++ exo_14.rb
@@ -4,17 +4,21 @@ y = gets.chomp.to_i
 
 y.times do |index|
 if (index < 10)
-    puts "jean.dupont.0#{index+1}@email.fr" 
+    z= "jean.dupont.0#{index+1}@email.fr" 
+        if (index+1).even? == true #.even? équivaut à %2 == 0
+    puts z
+    end
 else
-	puts "jean.dupont.#{index+1}@email.fr"
+	z= "jean.dupont.#{index+1}@email.fr"
+    if (index+1).even? == true
+    puts z
+    end
 end
-z="jean.dupont.#{index+1}@email.fr"
+
 x.push(z)
 end
 
 puts ""
-puts "Voici ceux contenant un nombre pair :"
-x.each {|mail| 
-if (mail[12]+mail[13]).to_i.even? #even permet de choisir les nombres pairs
-then puts mail
-end}
+puts "Rappel du tableau :"
+
+puts x

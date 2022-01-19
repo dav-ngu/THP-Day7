f76b8e62408322e0750c845b738ccc0c6d833aa6
davnguyen <dav-ngu31@hotmail.fr>
2022-01-19 16:18:35 +0100

    petites corrections

--- exo_14.rb
+++ exo_14.rb
@@ -3,7 +3,7 @@ puts "Nombre d'emails à générer :"
 y = gets.chomp.to_i
 
 y.times do |index|
-if (index < 10)
+if (index < 9)
     z= "jean.dupont.0#{index+1}@email.fr" 
         if (index+1).even? == true #.even? équivaut à %2 == 0
     puts z
